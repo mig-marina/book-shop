d6d5395b9e93e3190fa8b0c650c438b70725d802
Maryna Migacheva <m.migacheva@gmail.com>
2021-03-01 15:12:21 +0300

    feat: add Cart and CartItem components

--- bookShop/src/app/app.module.ts
+++ bookShop/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    CartComponent,
+    CartItemComponent
   ],
   imports: [
     BrowserModule
