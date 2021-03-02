34b0f78b726f93f041f62d24995c2816a0938c2d
Maryna Migacheva <m.migacheva@gmail.com>
2021-03-02 09:34:47 +0300

    feat: add solution from Cart and ItemCart

---
+++ bookShop/src/app/books.ts
@@ -0,0 +1,36 @@
+import { IBook } from './ibook';
+
+export const BOOKS: IBook[] = [
+  {
+    name: 'The ABC Murders',
+    description: 'Agatha Christie. "Murder by Alphabet" is a novel where Agatha Christie throws a new riddle to her favorite Hercule Poirot - letters and a railway directory, according to the alphabet of which murders take place in different cities. A lover of details, an attentive listener and observer, the great detective removes suspicion from the innocent and unravels a complex matter.',
+    price: 23,
+    category: Categories.Detective,
+    createDate: 2019,
+    isAvailable: true,
+   },
+   {
+     name: 'The ABC Murders',
+     description: 'Agatha Christie. "Murder by Alphabet" is a novel where Agatha Christie throws a new riddle to her favorite Hercule Poirot - letters and a railway directory, according to the alphabet of which murders take place in different cities. A lover of details, an attentive listener and observer, the great detective removes suspicion from the innocent and unravels a complex matter.',
+     price: 80,
+     category: Categories.Detective,
+     createDate: 2020,
+     isAvailable: false,
+   },
+   {
+     name: 'The ABC Murders',
+     description: 'Agatha Christie. "Murder by Alphabet" is a novel where Agatha Christie throws a new riddle to her favorite Hercule Poirot - letters and a railway directory, according to the alphabet of which murders take place in different cities. A lover of details, an attentive listener and observer, the great detective removes suspicion from the innocent and unravels a complex matter.',
+     price: 41,
+     category: Categories.Detective,
+     createDate: 2016,
+     isAvailable: true,
+   },
+   {
+     name: 'The ABC Murders',
+     description: 'Agatha Christie. "Murder by Alphabet" is a novel where Agatha Christie throws a new riddle to her favorite Hercule Poirot - letters and a railway directory, according to the alphabet of which murders take place in different cities. A lover of details, an attentive listener and observer, the great detective removes suspicion from the innocent and unravels a complex matter.',
+     price: 56,
+     category: Categories.Detective,
+     createDate: 2020,
+     isAvailable: false,
+   }
+];
--- bookShop/src/app/cart-item/cart-item.component.ts
+++ bookShop/src/app/cart-item/cart-item.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { IBook } from './../ibook';
+
 
 @Component({
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.css']
 })
+
 export class CartItemComponent implements OnInit {
+
+  listBooks = [
+    {
+      name: 'The ABC Murders',
+      count: 1,
+      price: 23,
+     },
+     {
+       name: 'The other book',
+       count: 1,
+       price: 41,
+     },
+     {
+       name: 'The new book',
+       count: 1,
+       price: 11,
+     },
+     {
+       name: 'The super new book',
+       count: 1,
+       price: 73,
+     }
+  ];
+
+  count = 1;
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  addOne(itemBook) {
+    itemBook.count = itemBook.count + 1;
+  }
+
+  minusOne(itemBook) {
+    if(itemBook.count !== 0) {
+      itemBook.count = itemBook.count - 1;
+    }
+  }
+
+  deleteItem(itemBook) {
+    const index = this.listBooks.indexOf(itemBook);
+    this.listBooks.splice(index, 1);
   }
 
 }
