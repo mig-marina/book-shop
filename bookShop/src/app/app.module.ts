13f6e509dd033a86f33b90270e643f0f757450c9
Maryna Migacheva <m.migacheva@gmail.com>
2021-03-06 17:56:37 +0300

    feat: use Books module

--- bookShop/src/app/app.module.ts
+++ bookShop/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BooksModule } from './modules/books/books.module';
 import { CartModule } from './modules/cart/cart.module';
 
+
 import { AppComponent } from './app.component';
-import { BookComponent } from './components/book/book.component';
+// import { BookComponent } from './components/book/book.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CartItemComponent } from './components/cart-item/cart-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent,
+    // BookComponent,
     CartComponent,
     CartItemComponent
   ],
   imports: [
     BrowserModule,
+    BooksModule,
     CartModule
   ],
   providers: [],
--- bookShop/src/app/modules/books/books.module.ts
+++ bookShop/src/app/modules/books/books.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { BookComponent } from '../../components/book/book.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BookComponent
+  ],
+  exports: [
+    BookComponent
+  ],
   imports: [
     CommonModule
   ]
